cef375efe859bb3654a93095080820a09af40681
hashmap <hashmap@gmail.com>
2019-11-14 16:27:30 +0100

    Make imports in core crate more Rust 2018 idiomatic (#3112)
    We use crate renaming to remove grin prefix, as  result imports for util and keychain crates start with crate::
    which looks like a part of the current crate. This PR fixes it.
    Also some small improvements were made.
    If this approach works I'll replicate it to other crates.

--- core/src/core/hash.rs
+++ core/src/core/hash.rs
@@ -17,19 +17,16 @@
 //! Primary hash function used in the protocol
 //!
 
+use crate::ser::{
+	self, AsFixedBytes, Error, FixedLength, ProtocolVersion, Readable, Reader, Writeable, Writer,
+};
+use blake2::blake2b::Blake2b;
 use byteorder::{BigEndian, ByteOrder};
 use std::cmp::min;
 use std::convert::AsRef;
 use std::ops::Add;
 use std::{fmt, ops};
-
-use crate::blake2::blake2b::Blake2b;
-
-use crate::libtx::secp_ser;
-use crate::ser::{
-	self, AsFixedBytes, Error, FixedLength, ProtocolVersion, Readable, Reader, Writeable, Writer,
-};
-use crate::util;
+use util;
 
 /// A hash consisting of all zeroes, used as a sentinel. No known preimage.
 pub const ZERO_HASH: Hash = Hash([0; 32]);
@@ -258,7 +255,7 @@ impl<D: DefaultHashable, E: DefaultHashable> DefaultHashable for (D, E) {}
 impl<D: DefaultHashable, E: DefaultHashable, F: DefaultHashable> DefaultHashable for (D, E, F) {}
 
 /// Implement Hashed trait for external types here
-impl DefaultHashable for crate::util::secp::pedersen::RangeProof {}
+impl DefaultHashable for util::secp::pedersen::RangeProof {}
 impl DefaultHashable for Vec<u8> {}
 impl DefaultHashable for u8 {}
 impl DefaultHashable for u64 {}
